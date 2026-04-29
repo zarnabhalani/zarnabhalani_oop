cdd741308a604f2024318b2a9d826fff0bbb3cbb
zarnabhalani <zarna0806@gmail.com>
2026-04-29 21:42:49 +0530

    Create Practical5.java

---
+++ Practical5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter side a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter side b: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter side c: ");
+        double c = sc.nextDouble();
+        if (a + b > c && a + c > b && b + c > a) {
+            double s = (a + b + c) / 2;
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+            System.out.printf("Area of triangle: %.2f", area);
+        } else {
+            System.out.println("Invalid triangle");
+        }
+    }
+}
